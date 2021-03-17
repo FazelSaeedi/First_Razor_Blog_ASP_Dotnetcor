d7085a77d40b7fdf1332b34e0159b0d79b3a744e
Fazel Saeedi <fazels36@gmail.com>
2021-03-17 14:00:22 +0330

    Create ASP.netCore5 Project

---
+++ Razor_Blog/Razor_Blog/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Razor_Blog.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            ViewData["message"] = "hellow World!";
+        }
+
+        public void OnPost(IFormCollection form)
+        {
+
+        }
+
+        public IActionResult OnGetLoad()
+        {
+            return Page();
+        }
+    }
+}
